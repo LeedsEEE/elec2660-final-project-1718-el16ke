ce9dcc50b11cf1ee69448f2509ce0f4a1850c1e9
Kaan Esendag <kaanesendag@Kaans-Mac.local>
2017-12-01 21:28:55 +0000

    More Updates on Datamodel, finished up to section 4 of RuleBook

--- RFL Build a Robot/RulesDataModel.m
+++ RFL Build a Robot/RulesDataModel.m
@@ -16,16 +16,79 @@
     if (self){
         
         
-/////////////////////////////////////////////////////////////////////section 1
+/////////////////////////////////////////////////////////////////////section 1///////////////////////////////////////////
         self.GeneralRules = [NSMutableArray array];
+        
         RulesPush *Teams = [[RulesPush alloc] init];
         Teams.RulesSubSection_0 = @"Team Members";
-        Teams.RulesContent_01 = @"At least one member of each team must be a student, or staff at a university.";
         Teams.RulesSubSection_1 = @"Team Size";
-        Teams.RulesContent_10 = @"A team must have no more than four official members per competing robot";
-        Teams.RulesContent_11 = @"A team must have no more than four official members per competing robot";
+        Teams.SubSectionTitle = @"Teams";
         
-    
+        RulesPush *RuleEnforcement = [[RulesPush alloc] init];
+        RuleEnforcement.RulesSubSection_0 = @"Dissassembly";
+        RuleEnforcement.SubSectionTitle = @"Rule Encforcement";
+        
+        RulesPush *RuleChanges = [[RulesPush alloc] init];
+        RuleChanges.RulesSubSection_0 = @"Notice";
+        RuleChanges.SubSectionTitle = @"Rule Changes";
+////////////////////////////////////////////////////////////////////Section 2///////////////////////////////////////////
+        self.PhysicalLimitations = [NSMutableArray array];
+        
+        RulesPush *SizeLimit = [[RulesPush alloc] init];
+        SizeLimit.RulesSubSection_0 = @"General Sizing";
+        SizeLimit.SubSectionTitle = @"Size Limit";
+        
+        RulesPush *WeightLimit = [[RulesPush alloc] init];
+        WeightLimit.RulesSubSection_0 = @"General Weight";
+        WeightLimit.RulesSubSection_1 = @"Metal Weight";
+        WeightLimit.SubSectionTitle = @"Weight Limit";
+////////////////////////////////////////////////////////////////////Section 3///////////////////////////////////////////
+        self.Materials = [NSMutableArray array];
+        
+        RulesPush *GeneralMaterial = [[RulesPush alloc] init];
+        GeneralMaterial.RulesSubSection_0 = @"Unspecifed materials";
+        GeneralMaterial.SubSectionTitle = @"General Material Rules";
+        
+        RulesPush *FullyAllowedMaterials = [[RulesPush alloc] init];
+        FullyAllowedMaterials.RulesSubSection_0 = @"Wood";
+        FullyAllowedMaterials.SubSectionTitle = @"Fully Allowed Materials";
+        
+        RulesPush *AllowedMetals = [[RulesPush alloc] init];
+        AllowedMetals.RulesSubSection_0 = @"Aluminium";
+        AllowedMetals.RulesSubSection_1 = @"Steel";
+        AllowedMetals.SubSectionTitle = @"Alloed Metals";
+        
+        RulesPush *AllowedPlastic = [[RulesPush alloc] init];
+        AllowedPlastic.RulesSubSection_0 = @"Acrylic";
+        AllowedPlastic.RulesSubSection_1 = @"Polystyrene";
+        AllowedPlastic.RulesSubSection_2 = @"Foam";
+        AllowedPlastic.RulesSubSection_3 = @"Polycarbonate";
+        AllowedPlastic.RulesSubSection_4 = @"ABS";
+        AllowedPlastic.RulesSubSection_5 = @"PLA";
+        AllowedPlastic.RulesSubSection_6 = @"HDPE";
+        AllowedPlastic.RulesSubSection_7 = @"Polystyrene";
+        AllowedPlastic.RulesSubSection_8 = @"Carbon Fiber";
+        AllowedPlastic.SubSectionTitle = @"Allowed Plastic";
+        
+        RulesPush *BannedMaterials = [[RulesPush alloc] init];
+        BannedMaterials.RulesSubSection_0 = @"Glass";
+        BannedMaterials.RulesSubSection_1 = @"Ceramics";
+        BannedMaterials.SubSectionTitle = @"Banned Materials";
+////////////////////////////////////////////////////////////////////Section 4///////////////////////////////////////////
+        self.Connection = [NSMutableArray array];
+        
+        RulesPush *GeneralConnectivity = [[RulesPush alloc] init];
+        GeneralConnectivity.RulesSubSection_0 = @"Frequency Range";
+        GeneralConnectivity.SubSectionTitle = @"General Connectivity";
+        
+        RulesPush *ConnectionDangers = [[RulesPush alloc] init];
+        ConnectionDangers.RulesSubSection_0 = @"Interference";
+        ConnectionDangers.RulesSubSection_1 = @"Failsafes";
+        ConnectionDangers.RulesSubSection_2 = @"Autonomy";
+        ConnectionDangers.SubSectionTitle = @"Connection Dangers";
+        
+        
+        
         
         
         
